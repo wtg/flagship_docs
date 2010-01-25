0c659ebfecc9b5e98c469e25b000d3d75e305b03
Brian M <michab3@rpi.edu>
2010-01-25 17:54:23 -0500

    Fixing group owner permissions

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ActiveRecord::Base
   has_and_belongs_to_many :users
 	belongs_to :leader, :class_name => 'User', :foreign_key => 'group_leader_id'
 
-	has_owner :leader
+	has_owner :group_leader
 	authenticates_creation :with_accessor_method => :is_admin
 	authenticates_saves :with => :allow_owner
 	authenticates_saves :with_accessor_method => :is_admin
