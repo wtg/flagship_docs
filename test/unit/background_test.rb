ae65d16bc6779ca78ea49e6a5bdd170d0eec43af
Brian M <michab3@rpi.edu>
2010-01-30 17:01:15 -0500

    Adding background tests, cleaning up category and document tests with setup method.

--- test/unit/background_test.rb
+++ test/unit/background_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class BackgroundTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :users
+
+  def setup
+    @regular = users(:regular)
+    @groupie = users(:groupie)
+    @admin = users(:admin)
+  end
+
+  def test_validation
+    background = Background.new()
+    assert !background.valid?
+
+    image = File.open("#{RAILS_ROOT}/test/binaries/background_1.jpg")
+    background.image = image
+    assert background.valid?
+  end
+
+  def test_authenticates_access
+    background = Background.new()
+    image = File.open("#{RAILS_ROOT}/test/binaries/background_1.jpg")
+    background.image = image
+
+    assert !background.save
+
+    ActiveRecord::Base.accessor = @regular
+    assert !background.save
+
+    ActiveRecord::Base.accessor = @groupie
+    assert background.save
   end
 end
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -3,6 +3,16 @@ require 'test_helper'
 class CategoryTest < ActiveSupport::TestCase
   fixtures :users, :groups, :categories
 
+  def setup
+    @groupie = users(:groupie)
+    @patron = users(:patron)
+    @regular = users(:regular)
+    @admin = users(:admin)
+
+    @cool_kids = groups(:cool_kids)
+    @patroons = groups(:patroons)
+  end
+
   def test_validation
     category = Category.new
     assert !category.valid?, "New category with no params"
@@ -11,15 +21,15 @@ class CategoryTest < ActiveSupport::TestCase
     assert !category.valid?, "New category with blank params"
 
     category.name = "Root 1"
-    category.group = groups(:cool_kids)
-    category.user = users(:groupie)
+    category.group = @cool_kids
+    category.user = @groupie
     assert category.valid?, "New category with params and owners"
   end
 
   def test_fields
     cat = Category.new({:name => "Root Level", :description => "Root Level Category"})
-    cat.group = groups(:cool_kids)
-    cat.user = users(:groupie)
+    cat.group = @cool_kids
+    cat.user = @patron
 
     assert_equal cat.name, "Root Level", "Category name retrieval"
 
@@ -32,10 +42,10 @@ class CategoryTest < ActiveSupport::TestCase
     ActiveRecord::Base.accessor = nil #Required to reset
 
     cat = Category.new({:name => "Root Level", :description => "Root Level Category"})
-    cat.group = groups(:cool_kids)
+    cat.group = @cool_kids
     assert !cat.save, "Category requires user id to save"
 
-    cat.user = users(:groupie)
+    cat.user = @groupie
     assert cat.save, "Category saves."
 
     roots = Category.roots
@@ -44,47 +54,35 @@ class CategoryTest < ActiveSupport::TestCase
   end
 
   def test_cat_with_authenticates_access
-    groupie = users(:groupie)
-    patron = users(:patron)
-    regular = users(:regular)
-    
-    ActiveRecord::Base.accessor = groupie
+    ActiveRecord::Base.accessor = @groupie
 
     cat = Category.new({:name => "Root Level", :description => "Root Level Category"})
-    cat.group = groups(:cool_kids)
+    cat.group = @cool_kids
     if cat.save
       #Testing autosets owner on create
-      assert_equal cat.user, groupie, "The owner was autoset when created"
+      assert_equal cat.user, @groupie, "The owner was autoset when created"
     else
       flunk "Test failed to save"
     end
     
     #Patron is in cool kids
-    ActiveRecord::Base.accessor = patron
+    ActiveRecord::Base.accessor = @patron
     assert cat.current_user_in_my_group?, "Current user in group for patron/cool_kids"
     
     #Regular is not
-    ActiveRecord::Base.accessor = regular
+    ActiveRecord::Base.accessor = @regular
     assert !cat.current_user_in_my_group?, "Current user in group for a user in no groups"
   end
   
   def test_cat_with_permissions
-    groupie = users(:groupie)
-    patron = users(:patron)
-    regular = users(:regular)
-    admin = users(:admin)
-    
-    cool_kids = groups(:cool_kids)
-    patroons = groups(:patroons)
-    
     #Public Category
-    ActiveRecord::Base.accessor = groupie
-    cat = Category.new({:name => "Root Level", :description => "Root Level Category", :group_id => patroons.id})
+    ActiveRecord::Base.accessor = @groupie
+    cat = Category.new({:name => "Root Level", :description => "Root Level Category", :group_id => @patroons.id})
     cat.save
     
     assert_equal cat.private, !cat.not_private, "Category not_private method"
     
-    [groupie, patron, regular, admin, nil].each do |u|
+    [@groupie, @patron, @regular, @admin, nil].each do |u|
       ActiveRecord::Base.accessor = u
       assert cat.allowed_to_read, "User allowed to read default category"
     end
@@ -92,46 +90,46 @@ class CategoryTest < ActiveSupport::TestCase
       ActiveRecord::Base.accessor = u
       assert !cat.allowed_to_read, "No one cannot read a default category"
     end
-    [groupie, patron, admin].each do |u|
+    [@groupie, @patron, @admin].each do |u|
       ActiveRecord::Base.accessor = u
       assert cat.allowed_to_save, "Owner, Group, Admin allowed to write default category"
       assert cat.save, "Default save ok"
     end
-    [regular, nil].each do |u|
+    [@regular, nil].each do |u|
       ActiveRecord::Base.accessor = u
       assert !cat.allowed_to_save, "Regular and nil cannot write default category"
       assert !cat.save, "Default save nogo"
     end
     
     #Hidden Category
-    ActiveRecord::Base.accessor = groupie
+    ActiveRecord::Base.accessor = @groupie
     cat.private = true
     cat.save
-    [groupie, patron, admin].each do |u|
+    [@groupie, @patron, @admin].each do |u|
       ActiveRecord::Base.accessor = u
       assert cat.allowed_to_read, "Owner, group members, and admin can read hidden category"
     end
-    [regular, nil].each do |u|
+    [@regular, nil].each do |u|
       ActiveRecord::Base.accessor = u
       assert !cat.allowed_to_read, "Outsiders cannot read hidden category"
     end
-    [groupie, patron, admin].each do |u|
+    [@groupie, @patron, @admin].each do |u|
       ActiveRecord::Base.accessor = u
       assert cat.allowed_to_save, "Owner, Group, Admin allowed to write hidden category"
       assert cat.save, "Hidden save ok"
     end
-    [regular, nil].each do |u|
+    [@regular, nil].each do |u|
       ActiveRecord::Base.accessor = u
       assert !cat.allowed_to_save, "Regular and nil cannot write hidden category"
       assert !cat.save, "Hidden save nogo"
     end
     
     #Publicly Writable Category
-    ActiveRecord::Base.accessor = groupie
+    ActiveRecord::Base.accessor = @groupie
     cat.private = false
     cat.writable = true
     cat.save
-    [groupie, patron, regular, admin, nil].each do |u|
+    [@groupie, @patron, @regular, @admin, nil].each do |u|
       ActiveRecord::Base.accessor = u
       assert cat.allowed_to_read, "Everyone can read public category"
     end
@@ -139,7 +137,7 @@ class CategoryTest < ActiveSupport::TestCase
       ActiveRecord::Base.accessor = u
       assert !cat.allowed_to_read, "No one cannot read public category"
     end
-    [groupie, patron, regular, admin].each do |u|
+    [@groupie, @patron, @regular, @admin].each do |u|
       ActiveRecord::Base.accessor = u
       assert cat.allowed_to_save, "All users can write public category"
       assert cat.save, "Public save ok"
