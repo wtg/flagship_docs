9ea130f071473cb16c52dbb052e44d9e4a097dff
Brian R Zaik <zaikb@rpi.edu>
2010-01-31 22:44:49 -0500

    Added default search BG image for JS-enabled version

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,15 @@
+var active_color = '#000'; // color of user provided text
+var inactive_color = '#ccc'; // color of default text
+
 $(document).ready(function () {
 	//Search box live search
+	$("#search_query").addClass("search-default");
+	
 	$("#search-jQuery").css({'display' : 'block'});
+	$("#search_query").click(function() {
+		$("#search_query").removeClass("search-default");
+	});
+	
 	$("#search_query").bind('keyup',function () {
 		if($(this).val().length == 0){
 			$("#search_query_auto_complete").fadeOut(100);
@@ -18,10 +27,27 @@ $(document).ready(function () {
 			});
 		}
 	});
-
+	
 	$('#searchbox'.parent).click(function() {
 		$("#search_query_auto_complete").fadeOut(300);
 	});
+
+	$('input#search_query').keypress(function() {
+		$('a#search_ex').fadeIn('fast');
+	});
+	
+	if (!$('input#search_query').val('')) {
+		$('a#search_ex').show();
+	}
+	
+	//Clear search box when click:
+	$('a#search_ex').click(function() {
+		$('input#search_query').val('');
+		$(this).fadeOut('fast');
+		$("#search_query_auto_complete").fadeOut(100);
+		$("#search_query").addClass("search-default");
+	});
+	
 	//End search box
 
 	//Expandable category tree
@@ -41,20 +67,6 @@ $(document).ready(function () {
 	});
 	//End category tree
 	
-	$('input#search_query').keypress(function() {
-		$('a#search_ex').fadeIn('fast');
-	});
-	
-	if (!$('input#search_query').val('')) {
-		$('a#search_ex').show();
-	}
-	
-	//Clear search box when click:
-	$('a#search_ex').click(function() {
-		$('input#search_query').val('');
-		$(this).fadeOut('fast');
-		$("#search_query_auto_complete").fadeOut(100);
-	});
 	
 	$('.info_box p img').tipsy({gravity: 's'});
 	$('ul.img_sel li img').tipsy({gravity: 's'});
