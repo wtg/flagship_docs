a5849849cb27a01be5e5cea01b7d40134b14f85b
Brian M <michab3@rpi.edu>
2010-02-01 17:40:01 -0500

    Whoops, duplicate index.

--- db/migrate/20100201142950_add_indexes_to_revisions.rb
+++ db/migrate/20100201142950_add_indexes_to_revisions.rb
@@ -2,12 +2,10 @@ class AddIndexesToRevisions < ActiveRecord::Migration
   def self.up
     add_index :revisions, :document_id, {:name => "document_id_index"}
     add_index :revisions, :user_id, {:name => "user_id_index"}
-    add_index :revisions, :position, {:name => "position_index"}
   end
 
   def self.down
     remove_index :revisions, :document_id_index
     remove_index :revisions, :user_id_index
-    remove_index :revisions, :position_index
   end
 end
