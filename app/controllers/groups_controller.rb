df3f746d1b611b33551d71b24614ce212fb027ce
Brian M <michab3@rpi.edu>
2010-02-02 00:08:16 -0500

    Updating permissions on groups.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,7 @@ class GroupsController < ApplicationController
   # GET /groups.xml
   def index
     @groups = Group.all
+    @groups.delete_if {|x| !x.allowed_to_read}
 
     respond_to do |format|
       format.html # index.html.erb
@@ -14,59 +15,81 @@ class GroupsController < ApplicationController
   # GET /groups/1.xml
   def show
     @group = Group.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @group }
+    if @group.allowed_to_read
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @group }
+      end
+    else
+      flash[:error] = "Sorry, the page you requested in unavailable."
+      redirect_back
     end
   end
 
   # GET /groups/new
   # GET /groups/new.xml
   def new
-    @group = Group.new
+    if Group.allowed_to_create
+      @group = Group.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @group }
+      respond_to do |format|
+        format.html # new.html.erb
+        format.xml  { render :xml => @group }
+      end
+    else
+      flash[:error] = "Sorry, the page you requested in unavailable."
+      redirect_back
     end
   end
 
   # GET /groups/1/edit
   def edit
     @group = Group.find(params[:id])
+    if !@group.allowed_to_save
+      flash[:error] = "Sorry, the page you requested in unavailable."
+      redirect_back
+    end
   end
 
   # POST /groups
   # POST /groups.xml
   def create
-    @group = Group.new(params[:group])
+    if !Group.allowed_to_save
+      flash[:error] = "Sorry, the page you requested in unavailable."
+      redirect_back
+    else
+      @group = Group.new(params[:group])
 
-    respond_to do |format|
-      if @group.save
-        flash[:notice] = 'Group was successfully created.'
-        format.html { redirect_to(@group) }
-        format.xml  { render :xml => @group, :status => :created, :location => @group }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @group.errors, :status => :unprocessable_entity }
-      end
-    end
+      respond_to do |format|
+        if @group.save
+          flash[:notice] = 'Group was successfully created.'
+          format.html { redirect_to(@group) }
+          format.xml  { render :xml => @group, :status => :created, :location => @group }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @group.errors, :status => :unprocessable_entity }
+        end
+     end
+   end
   end
 
   # PUT /groups/1
   # PUT /groups/1.xml
   def update
     @group = Group.find(params[:id])
-
-    respond_to do |format|
-      if @group.update_attributes(params[:group])
-        flash[:notice] = 'Group was successfully updated.'
-        format.html { redirect_to(@group) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @group.errors, :status => :unprocessable_entity }
+    if !@group.allowed_to_save
+      flash[:error] = "Sorry, the page you requested in unavailable."
+      redirect_back
+    else
+      respond_to do |format|
+        if @group.update_attributes(params[:group])
+          flash[:notice] = 'Group was successfully updated.'
+          format.html { redirect_to(@group) }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @group.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -74,12 +97,17 @@ class GroupsController < ApplicationController
   # DELETE /groups/1
   # DELETE /groups/1.xml
   def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
+    if !admin_logged_in?
+      flash[:error] = "Sorry, the page you requested in unavailable."
+      redirect_back
+    else
+      @group = Group.find(params[:id])
+      @group.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(groups_url) }
-      format.xml  { head :ok }
+      respond_to do |format|
+        format.html { redirect_to(groups_url) }
+        format.xml  { head :ok }
+      end
     end
   end
 end
