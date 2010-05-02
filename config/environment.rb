f49f76bed6fd5e804de7d1ef50e6de22557b269d
Zach Rowe <zrowe007@gmail.com>
2010-05-02 00:20:57 -0400

    added time zone to allow for correct time display

--- config/environment.rb
+++ config/environment.rb
@@ -40,4 +40,5 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  config.time_zone = "Eastern Time (US & Canada)"
 end
