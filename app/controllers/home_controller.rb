b1168dd582776c01020e2f503a021c6c6a3f2717
Brian M <michab3@rpi.edu>
2010-05-07 13:51:37 -0400

    Adding some RSS to the homepage.  [#24 state:resolved]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,16 @@
 class HomeController < ApplicationController
+  # GET /index
+  # GET /index.xml
   def index
     @featured_categories = Category.find( :all, :conditions => { :is_featured => true } )
     @featured_categories.delete_if {|x| !x.allowed_to_read} #Hide Private Featured Categories
+
     @categories = Category.roots
-	@featured_categories.delete_if {|x| !x.allowed_to_read} #Hide Private Root Categories
     @categories.delete_if {|x| !x.allowed_to_read} #Hide Private Root Categories
+
     respond_to do |format|
       format.html # index.html.erb
+      format.rss
       format.xml  { render :xml => @categories }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -52,4 +52,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action.:format'
 end
