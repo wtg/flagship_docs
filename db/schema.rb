1ed7cac07cfe9b011a0f1c4ec2b29fd4f74344af
Brian M <michab3@rpi.edu>
2010-08-23 12:53:29 -0400

    Undoing some of Haris's commit.  Sessions no longer in the DB for now.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100817031608) do
+ActiveRecord::Schema.define(:version => 20100607040352) do
 
   create_table "backgrounds", :force => true do |t|
     t.string   "image_file_name"
@@ -91,16 +91,6 @@ ActiveRecord::Schema.define(:version => 20100817031608) do
   add_index "revisions", ["document_id"], :name => "document_id_index"
   add_index "revisions", ["position"], :name => "position_index"
   add_index "revisions", ["user_id"], :name => "user_id_index"
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "username"
