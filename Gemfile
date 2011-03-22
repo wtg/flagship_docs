5872015f0e3f5316dbdb4353bfe5cda421be3dc3
Brian M <michab3@rpi.edu>
2011-03-22 12:44:36 -0400

    Base Rails 3 application.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+gem 'sqlite3'
+
+# Deploy with Capistrano
+# gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docs::Application.config.secret_token = '30cc76ba34a353d23cd65fa8e1fe9f52504d49178c01ff0c79acff229b3d481acb7b9e1bdb37abdcf4579b45cec397c0e65152535d8f702247b6dbf8cb9eff3d'
