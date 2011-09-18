7f9005827f2f784349e07999cbe233af84cd20dc
Brian Michalski <bmichalski@gmail.com>
2011-09-17 17:14:12 -0700

    Remove the html index page, add root route.

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Docs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "categories#index"
 
   # See how all your routes lay out with "rake routes"
 
