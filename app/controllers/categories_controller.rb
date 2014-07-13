a182e9759c0874586b97bf49ab0ae4b4cf4016a2
Gabe Perez <perez283@gmail.com>
2014-07-13 01:59:23 -0400

    Create categories and initial work on browsing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,4 +9,23 @@ class CategoriesController < ApplicationController
     @category = Category.find params[:id]
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      redirect_to "/"
+    end
+  end   
+
+  private
+    def category_params
+      params.require(:category).permit(:name, :description, 
+        :group_id, :is_featured, :private)
+    end 
+
 end
